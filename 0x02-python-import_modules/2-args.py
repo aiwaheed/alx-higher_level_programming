0373169adaed34b5a4f5eaa4534e3d334168ebfd
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-22 22:00:53 +0100

    commitx-main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    args = len(argv)
-    num = 1
-    if (args == 1):
-        print("{} arguments.".format(args - 1))
-    elif (args > 1):
-        i = '' if args == 2 else 'i'
-        print("{} argument{}:".format(args - 1, i))
-        while num < args:
-            print("{}: {}".format(num, argv[num]))
-            num += 1
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print(f"{len(sys.argv) - 1} arguments:")
+        for i in range(1, len(sys.argv)):
+            print('{}: {}'.format(i, sys.argv[i]))
