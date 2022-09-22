83583f8cd1eb86b137ed08372310e548c8768d83
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-22 20:39:06 +0100

    commitx-main

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    num_args = len(argv)
-    total = 0
-    for i in range(1, num_args):
-        total += int(argv[i])
-        print("{:d}".format(total))
+    import sys
+    add = 0
+    for i in range(len(sys.argv) - 1):
+        add += int(sys.argv[i + 1])
+        print(f"{add}")
