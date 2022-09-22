a3b36e0d8d13fcfc31d1a58a34ea3cb24eb0e8c4
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-22 22:24:44 +0100

    commitx-main

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,37 +1,26 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
+if __name__ == "__main__":  
+    import sys
     from calculator_1 import add, sub, mul, div
-    count = len(argv)
-    if count != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+    arglen = (len(sys.argv))-1
+    if arglen != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if (sys.argv[2] == "+"):
+        result = add(a,b)
+    elif (sys.argv[2] == "-"):
+        result = sub(a, b)
+    elif (sys.argv[2] == "*"):
+        result = mul(a, b)
+    elif(sys.argv[2] == "/"):
+        result = div(a,b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and")
+                                                                          
+
         exit(1)
-        num1 = int(argv[1])
-        num2 = int(argv[3])
-        op = argv[2]
-        def not_found():
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-            def my_add():
-                total = add(num1, num2)
-                print("{:d} + {:d} = {:d}".format(num1, num2, total))
-                return total
-            def my_sub():
-                total = sub(num1, num2)
-                print("{:d} - {:d} = {:d}".format(num1, num2, total))
-                return total
-            def my_mul():
-                total = mul(num1, num2)
-                print("{:d} * {:d} = {:d}".format(num1, num2, total))
-                return total
-            def my_div():
-                total = div(num1, num2)
-                print("{:d} / {:d} = {:d}".format(num1, num2, total))
-                return total
-            options = {
-                    "+": my_add,
-                    "-": my_sub,
-                    "*": my_mul,
-                    "/": my_div
-                    }
-            options.get(op, not_found)()
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
