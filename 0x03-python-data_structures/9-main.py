522b2abbeb9d695e0a552e4f9ccb5bd10d74fca0
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-27 00:57:02 +0100

    commitz

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -1,4 +1,5 @@
-nteger = __import__('9-max_integer').max_integer
+#!/usr/bin/python3
+print_list_integer = __import__('9-max_integer').max_integer
 
         my_list = [1, 90, 2, 13, 34, 5, -13, 3]
         max_value = max_integer(my_list)
