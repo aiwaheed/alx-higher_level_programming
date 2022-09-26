737c18bc9b082c3386bd7db1601ce65c50f63297
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-26 17:54:20 +0100

    commtn

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i == 'c' or i == 'C':
-            i = ""
-            new_string += i
+    if i == 'c' or i == 'C':
+        i = ""
+        new_string += i
     return new_string
