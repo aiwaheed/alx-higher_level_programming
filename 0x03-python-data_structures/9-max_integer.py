16b67febd1047787c3202667099a0251e80cfee6
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-27 17:00:17 +0100

    commitsx

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,6 @@ def max_integer(my_list=[]):
     if my_list == []:
         return None
     else:
-        maxi = my_list[0]
-        for i in range(len(my_list)):
-            if maxi < my_list[i]:
-                maxi = my_list[i]
-                return maxi
+        my_list.sort()
+        max_v = my_list.pop()
+        return max_v
