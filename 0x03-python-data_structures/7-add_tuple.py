2fdc09fde1552f316de961f1d832282d290fc20b
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-27 01:14:46 +0100

    commitz

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = list(tuple_a)
-    b = list(tuple_b)
-    for i in range(len(a)):
-        if len(a) < 2:
-            a.append(0)
-    for i in range(len(b)):
-        if len(b) < 2:
-            b.append(0)
-        if len(a) == 0:
-            a = [0, 0]
-        elif len(b) == 0:
-            b = [0, 0]
-            Sum = (a[0] + b[0], a[1] + b[1])
-            totalSum = tuple(Sum)
-    return 
+    a = list(tuple_a) + [0]*2
+    b = list(tuple_b) + [0]*2
+    c = [x + y for x, y in zip(a, b)]
+    return tuple(c[0:2])
