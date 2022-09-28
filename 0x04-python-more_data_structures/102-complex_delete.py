8d717adeacdac54adce7985d96f2727f9e977824
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-28 23:01:13 +0100

    comt

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,6 @@
 def complex_delete(my_dict, value):
     copy = my_dict.copy()
     for k, v in copy.items():
-    if value in v:
+        if value in v:
         del my_dict[k]
-        return my_dict
+    return my_dict
