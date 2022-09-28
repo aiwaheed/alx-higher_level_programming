a9b54710c00bd8dd50bc09f38e293bb111d1bd78
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-28 22:55:20 +0100

    comt

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,6 @@
 def weight_average(my_list=[]):
     if len(my_list) is 0:
         return 0
-        a = list(map(list, zip(*my_list)))
-        b = [x * y for x, y in zip(a[0], a[1])]
-        return sum(b) / sum(a[1])
+    a = list(map(list, zip(*my_list)))
+    b = [x * y for x, y in zip(a[0], a[1])]
+    return sum(b) / sum(a[1])
