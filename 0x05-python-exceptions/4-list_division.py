6606c90e640359744b2eeb5f315853983fe392d8
aiwaheed <a.waheedibrahim@gmail.com>
2022-10-11 00:45:01 +0100

    Commit exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""
-a function that divides element by element 2 lists
-"""
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
-    div_elm = 0
+    store = []
+    switch = 0
     for i in range(list_length):
         try:
-            div_elm = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("wrong type")
-            div_elm = 0
+            x = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            div_elm = 0
+            switch = 1
+        except TypeError:
+            print("wrong type")
+            switch = 1
         except IndexError:
             print("out of range")
-            div_elm = 0
+            switch = 1
         finally:
-            new_list.append(div_elm)
-            return new_list
+            if switch:
+                switch = 0
+                store.append(0)
+            else:
+                store.append(x)
+                return store
