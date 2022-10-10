2c0235c9827e591bcdbf36e796a2d01dd7193fea
aiwaheed <a.waheedibrahim@gmail.com>
2022-10-10 23:52:24 +0100

    Commit exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    while True:
-        try:
-    if count < x:
-        print(my_list[count], end='')
-        count += 1
-    else:
-        print()
-        return count
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
     except IndexError:
-        print()
-        return count
+        return i
+    else:
+        return x
+    finally:
+        print("")
