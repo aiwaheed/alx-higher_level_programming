751c26360aa91b48f1677cdd11d73add8dccc707
aiwaheed <a.waheedibrahim@gmail.com>
2022-10-11 21:29:39 +0100

    commit classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class with initialize size.
+"""
+class Square:
+    """A class that defines a Square by size"""
+    def __init__(self, size):
+        self.__size = size
+        Footer
+        © 2022 GitHub, Inc.
+        Footer navigation
+        Terms
+
